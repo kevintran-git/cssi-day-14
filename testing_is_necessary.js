4f541e243e3540450cb6ed268db5bbb00eee6214
Kevin Tran <kinzerfest@gmail.com>
2021-07-29 12:23:26 -0500

    Update testing_is_necessary.js

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -12,6 +12,12 @@ funAnimals.push({
     reason: "four legs worth of energy at the cost of three"
 })
 
+funAnimals.push({
+    name: "burger",
+    type: "turtle",
+    reason: "burger shaped"
+})
+
 for (let animal of funAnimals) {
     console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
 }
